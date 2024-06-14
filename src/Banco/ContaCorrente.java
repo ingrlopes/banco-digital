b3dd8d1f7015225011efc45de7f56744d3c0fc67
Ingrid Lopes <ingridlopessudario27@icloud.com>
2024-06-14 18:01:48 -0300

    Update

--- src/Banco/ContaCorrente.java
+++ src/Banco/ContaCorrente.java
@@ -1,5 +1,9 @@
 public class ContaCorrente extends Conta {
 
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
     public void extrato() {
         System.out.println(">>> Extrato Conta Corrente");
         super.imprimirExtrato();
--- src/Banco/ContaPoupanca.java
+++ src/Banco/ContaPoupanca.java
@@ -1,5 +1,9 @@
 public class ContaPoupanca extends Conta {
 
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+    }
+
     public void extrato() {
         System.out.println(">>> Extrato Conta Poupança");
         super.imprimirExtrato();
